ee2dc7761f2d99eb7e36b6ea8fb7ea83db962446
yahor.kachanau <yahor.kachanau@leverx.com>
2021-02-12 01:48:07 +0300

    fixed issue with imports

--- script.jsx
+++ script.jsx
@@ -1,5 +1,3 @@
-import "Panel.jsx"
-
 const REGIM = {
 	S: "Students",
 	C: "Companies",
@@ -1416,6 +1414,28 @@ class Navigation extends React.Component {
 			<div id="C" onClick={this.onClick.bind(this)} className="tab">Companies</div>
 			<div id="E" onClick={this.onClick.bind(this)} className="tab">Events</div>
 		</nav>
+	}
+}
+
+class Panel extends React.Component {
+	state = { currentStudent: null, currentCompany: null };
+	updateCurrentStudent = (student) => {
+		this.setState({ currentStudent: student });
+	}
+	updateCurrentCompany = (company) => {
+		this.setState({ currentCompany: company });
+	}
+	updateCurrentEvent = (event) => {
+		this.setState({ currentEvent: event });
+	}
+	render() {
+		return <div className="panel">
+			{(this.props.Regim === REGIM.S) && <Students data={dataStudents} updateCurrentStudent={this.updateCurrentStudent} currentStudent={this.state.currentStudent} />}
+			{(this.props.Regim === REGIM.S) && <Student_Events data={dataEvents} currentStudent={this.state.currentStudent} />}
+			{(this.props.Regim === REGIM.C) && <Companies data={dataCompanies} updateCurrentCompany={this.updateCurrentCompany} currentCompany={this.state.currentCompany} />}
+			{(this.props.Regim === REGIM.C) && <Company_Events data={dataEvents} currentCompany={this.state.currentCompany} />}
+			{(this.props.Regim === REGIM.E) && <Events data={dataEvents} updateCurrentEvent={this.updateCurrentEvent} currentEvent={this.state.currentEvent} />}
+		</div>
 	}
 }
 
