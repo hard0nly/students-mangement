83c5b7a8c5808a0d9293c2b652fd25789b64d704
yahor.kachanau <yahor.kachanau@leverx.com>
2021-02-12 01:29:54 +0300

    initial

---
+++ shared/Data_Companies.js
@@ -0,0 +1,8 @@
+const dataCompanies = [
+	{ id: 1, name: 'iTechArt' },
+	{ id: 2, name: 'LeverX' },
+	{ id: 3, name: 'iSsoft' },
+	{ id: 4, name: 'EPAM' },
+	{ id: 5, name: 'IBA' },
+	{ id: 6, name: 'Wargaming' }
+];
---
+++ shared/Data_Students.js
@@ -0,0 +1,10 @@
+const dataStudents = [
+	{ id: 1, name: 'Петров П.П.', spec: 'ИСиТ', group: 1, syear: 2016 },
+	{ id: 2, name: 'Иванов И.И.', spec: 'ИСиТ', group: 1, syear: 2016 },
+	{ id: 3, name: 'Сидоров С.С.', spec: 'ПОИТ', group: 4, syear: 2016 },
+	{ id: 4, name: 'Кириллов К.К.', spec: 'ПОИТ', group: 5, syear: 2016 },
+	{ id: 5, name: 'Алексеев А.А.', spec: 'ДЭВИ', group: 9, syear: 2016 },
+	{ id: 6, name: 'Сергеев С.С.', spec: 'ПОИБМС', group: 7, syear: 2017 },
+	{ id: 7, name: 'Михайлов М.М.', spec: 'ПОИБМС', group: 8, syear: 2017 },
+	{ id: 8, name: 'Александров А.А.', spec: 'ПОИБМС', group: 7, syear: 2017 }
+];
